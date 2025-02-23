8b8bccbb33c216e8d89a642b961367eced75d06a
Alison Gunzler <agunzle1@jhu.edu>
2025-02-23 17:39:00 -0500

    bfore fix

--- assignments/assignment_2/solution.py
+++ assignments/assignment_2/solution.py
@@ -29,115 +29,51 @@ def solve_padding_oracle(ctx, server):
     Returns:
         bytes: The recovered plaintext message with the padding removed.
     """
-
-    # get byte array for ease
-    # r = bytearray(ctx)
-    # b = len(r)
-    # block_sz = 16
-    # blocks = b // block_sz
-    # pt = list()
-    # iv = r[0:block_sz]
-
-    # for st in range(blocks -1, 0, -1) :
-    #     # 16 bytes in r
-    #     r1 = r[st*block_sz:st*block_sz+block_sz] #current block 
-    #     r2 = r[st*block_sz - block_sz:st*block_sz] #previous block
-
-    #     for by in range(block_sz-1,-1,-1) :
-    #         byte = 0
-    #         for i in range(256) :
-    #             # r1k = bytearray(r1)
-    #             r2k = bytearray(r2)
-
-    #             r2k[by] = r2k[by] ^ i
-    #             concat_lst = iv + r1 + r2k
-                
-    #             if(server(concat_lst) != 0) :
-    #                 byte = i
-    #                 print("here")
-    #                 break
-    #         pt.append(byte)
-    # for c in pt:
-    #      print(chr(c))
-
-    r = bytearray(ctx)
-    b = 16
+#     1. getting padding
+#     - xor 2nd bit by 1, see if still valid
+#     - cont
+# - know 0E -> make 14 by add 1
+#     - change 1st M by i=0-255 until get 14
+#     - MM = 0E xor XX
+#     - repeat with next MM
+    ct = bytearray(ctx)
+    n = len(ct)
+    num_padding = det_padding_count(ctx, server)
+    M_byte_ind = n-num_padding-1
     pt = list()
-    for st in range(len(r)//16 -1,0, -1) :
-        r1 = r[st*b:st*b+b] #current block 
-        r2 = r[st*b - b:st*b] #previous block
-        dec_blk = blk_dec_oracle(r1, server)
-        p_blk = [0] * b
-        for i in range(b):
-            p_blk[i] = dec_blk[i] ^ r2[i]
-        pt = p_blk + pt
+
+    while (n != num_padding) :
+        for i in range(num_padding) :
+            print("ok")
+            print(len(ct))
+            print(i)
+            print(n-i-1)
+            print(num_padding)
+            ct[n-i-1] = ct[n-i-1] ^ 1 #ADD 1??
+        xorfac = 0
+        for j in range(256) :
+            ct1 = bytearray(ct)
+            ct1[M_byte_ind] = ct1[M_byte_ind] ^ j
+            if(server(ct1) == 1) :
+                xorfac = j
+        ct[M_byte_ind] = ct[M_byte_ind] ^ xorfac
+        new_mbyte = xorfac ^ (num_padding ^ 1) #ADD 1??
+        pt.insert(0, new_mbyte)
+        print(pt)
+        n = n+1
     return bytes(pt)
 
 
-def blk_dec_oracle(r, server) :
-    print("1")
-
-    # 1. take rk = ak ⊕ (b − j + 2) for k = j, . . . , b
-    
-    a = last_wd_oracle(r, server)
-    j = len(a) #? HALP
-    b = 16
-
-    for k in range (j,b,1) :
-        
-        r[k] = a[k-j] ^ (b - j + 2)
-
-    # 2. pick r1, . . . , rj−1 at random and take i = 0
-
-    i = 0
-
-    # 3. take r = r1 . . . rj−2(rj−1 ⊕ i)rj . . . rb
-
-    rn = r[0:j-2] + [r[j-1] ^ i] + r[j:b]
-
-    # 4. if O(r|y) = 0 then increment i and go back to the previous step
-
-    while(server(rn) == 0) :
-        i = i+1
-        rn = r[0:j-2] + [r[j-1] ^ i] + r[j:b]
-
-    # 5. output rj−1 ⊕ i ⊕ (b − j + 2)
-    return r[j-1] ^ i ^ (b-j+2)
-
-def last_wd_oracle(r, server) :
-    #pick a few random words r1, . . . , rb and take i = 0
-    b=16
-    i=0
-    print(len(r))
-    print("h")
-    print(r[b-1] ^ i)
-    #pick r = r1 . . . rb−1(rb ⊕ i)
-    newl = [r[b-1] ^ i]
-    print(bytearray(newl))
-    print(len(r[0:b-1]))
-    rn = r[0:b-1] + bytearray(newl)
-    print(rn)
-    #if O(r|y) = 0 then increment i and go back to the previous step
-    while(server(rn) == 0) :
-        i = i+1
-        rn = r[0:b-1] + bytearray(r[b-1] ^ i)
-    #replace rb by rb ⊕ i
-    r[b-1] = r[b-1] ^ i
-    #for n = b down to 2 do
-    for n in range(b,2,-1) :
-        #take r = r1 . . . rb−n(rb−n+1 ⊕ 1)rb−n+2 . . . rb
-        r = r[0:b-n] + bytearray(r[b-n] ^ 1) + r[b-n+1:b]
-        #if O(r|y) = 0 then stop and output (rb−n+1 ⊕ n). . .(rb ⊕ n)
-        if(server(r) == 0) :
-            return r[b-n:b]
-    #output rb ⊕ 1
-    return r[b-1] ^ 1
-
-
-    
-
-
-
+def det_padding_count(ctx, server) :
+    ct = bytearray(ctx)
+    n = len(ct)
+    for i in range (0, 16) :
+        ct[n-1-i] = ct[n-1-i] ^ 1
+        if(server(ct) == 1) :
+            print(i)
+            return i
+        ct = bytearray(ctx)
+    return 0
 
 
 
